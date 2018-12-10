b1b7d59eac38b4d573826416eb3654b9b7478119
Valeska Halamicek <vjhalamicek@gmail.com>
2018-12-10 15:23:41 -0600

    finished assignment

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,23 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchType == "all")
+            {
+                List<Dictionary<string, string>> AllJobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = AllJobs;
+            }
+            else
+            {
+                List<Dictionary<string, string>> AllJobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = AllJobs;
+            }
+
+            return View("Index");
+        }
+
     }
 }
